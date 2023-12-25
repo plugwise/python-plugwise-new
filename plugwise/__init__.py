e1665adad13bdcd6ba3a7ca90e5e62a372f5cb5f
Bouwe <bouwe.s.westerdijk@gmail.com>
2023-12-25 10:24:43 +0100

    Correct again

--- plugwise/__init__.py
+++ plugwise/__init__.py
@@ -71,7 +71,9 @@ class SmileData(SmileHelper):
             if (
                 "binary_sensors" in device
                 and "plugwise_notification" in device["binary_sensors"]
-            ) or self._is_thermostat:
+            ) or (
+                device_id == self.gateway_id and self._is_thermostat
+            ):
                 data["binary_sensors"]["plugwise_notification"] = bool(
                     self._notifications
                 )
